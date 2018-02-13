d849165fda9ccbc5e4b192cd4f1a06c37d464a42
Alvaro Garcia <derecerca@gmail.com>
2018-02-13 20:18:01 +0100

    Add sample Spring boot app to satisfy the mvn goal

--- src/main/kotlin/com/gmaur/investment/robotadvisor/RobotAdvisorApp.kt
+++ src/main/kotlin/com/gmaur/investment/robotadvisor/RobotAdvisorApp.kt
@@ -1,5 +1,13 @@
 package com.gmaur.investment.robotadvisor
 
-class RobotAdvisorApp {
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
 
+@SpringBootApplication
+@EnableAutoConfiguration
+class RobotAdvisorApp
+
+fun main(args: Array<String>) {
+    runApplication<RobotAdvisorApp>(*args)
 }
