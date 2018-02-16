18fceef38d729420c107fcd8b7fac090de01b92c
Alvaro Garcia <derecerca@gmail.com>
2018-02-16 22:54:20 +0100

    #2 Unit green: validate RebalanceRequests

---
+++ src/test/kotlin/com/gmaur/investment/robotadvisor/RobotAdvisorAppShould.kt
@@ -0,0 +1,49 @@
+package com.gmaur.investment.robotadvisor
+
+import com.gmaur.investment.robotadvisor.domain.*
+import com.gmaur.investment.robotadvisor.infrastructure.RebalanceRequest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.ExpectedException
+import org.mockito.Mockito
+import java.math.BigDecimal
+
+class RobotAdvisorAppShould {
+    private val portfolioRebalancer: PortfolioRebalancer = Mockito.mock(PortfolioRebalancer::class.java)
+
+    @Rule
+    @JvmField
+    val expectedException: ExpectedException = ExpectedException.none()
+
+    private var robotAdvisorApp: RobotAdvisorApp? = null
+
+    @Before
+    fun setUp() {
+        robotAdvisorApp = RobotAdvisorApp(portfolioRebalancer)
+    }
+
+    @Test
+    fun `validate the rebalance request - no portfolio`() {
+        expectedException.expect(IllegalArgumentException::class.java)
+
+        robotAdvisorApp?.rebalance(correctRebalanceRequest.copy(current = null))
+
+        Mockito.verifyZeroInteractions(portfolioRebalancer)
+    }
+
+    @Test
+    fun `validate the rebalance request - no assetallocation`() {
+        expectedException.expect(IllegalArgumentException::class.java)
+
+        robotAdvisorApp?.rebalance(correctRebalanceRequest.copy(ideal = null))
+
+        Mockito.verifyZeroInteractions(portfolioRebalancer)
+    }
+
+    private val correctRebalanceRequest: RebalanceRequest = RebalanceRequest(
+            AssetAllocation(
+                    listOf(AssetAllocationSingle(ISIN("LU1"), Percentage("100")))),
+            Portfolio(listOf(Asset(ISIN("LU1"), Amount(BigDecimal.valueOf(100L)))))
+    )
+}
