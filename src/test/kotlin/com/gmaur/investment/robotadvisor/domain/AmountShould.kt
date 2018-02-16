c9f92a75230c23485ca8dc70748f2e8d20eb196b
Alvaro Garcia <derecerca@gmail.com>
2018-02-17 00:28:24 +0100

    Test Amounts

--- src/test/kotlin/com/gmaur/investment/robotadvisor/domain/AmountShould.kt
+++ src/test/kotlin/com/gmaur/investment/robotadvisor/domain/AmountShould.kt
@@ -9,6 +9,20 @@ class AmountShould {
     fun `calculate percentage`() {
         val result = Amount(BigDecimal.valueOf(10L)).percentageOf(Amount(BigDecimal.valueOf(100L)))
 
-        assertThat(result).isEqualTo(Percentage("0.1"))
+        assertThat(result).isEqualTo(Percentage("0.10"))
+    }
+
+    @Test
+    fun `add with decimals`() {
+        val result = Amount(BigDecimal("10.0")).add(Amount(BigDecimal("11")))
+
+        assertThat(result).isEqualTo(Amount(BigDecimal("21.00")))
+    }
+
+    @Test
+    fun `add without decimals`() {
+        val result = Amount(BigDecimal("10")).add(Amount(BigDecimal("11")))
+
+        assertThat(result).isEqualTo(Amount(BigDecimal("21.00")))
     }
 }
