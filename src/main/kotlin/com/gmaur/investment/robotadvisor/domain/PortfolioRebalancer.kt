c01ffb8d3d4b3aecc810ee797ef1adbfbe937ae5
Alvaro Garcia <derecerca@gmail.com>
2018-02-16 23:10:33 +0100

    #2 - Green

--- src/main/kotlin/com/gmaur/investment/robotadvisor/domain/PortfolioRebalancer.kt
+++ src/main/kotlin/com/gmaur/investment/robotadvisor/domain/PortfolioRebalancer.kt
@@ -4,6 +4,9 @@ import java.math.BigDecimal.valueOf
 
 open class PortfolioRebalancer {
     open fun rebalance(ideal: AssetAllocation, current: Portfolio): Operations {
+        if (ideal.matches(current)) {
+            return Operations(listOf())
+        }
         return Operations(listOf(Sell(Asset(ISIN("LU SELL"), Amount(valueOf(27L))))))
     }
 }
--- src/main/kotlin/com/gmaur/investment/robotadvisor/domain/domain.kt
+++ src/main/kotlin/com/gmaur/investment/robotadvisor/domain/domain.kt
@@ -21,6 +21,16 @@ data class ISIN(val value: String)
 
 data class Asset(val isin: ISIN, val amount: Amount)
 data class TransferrableAsset(val asset: Asset)
-data class AssetAllocation(val values: List<AssetAllocationSingle>)
+data class AssetAllocation(val values: List<AssetAllocationSingle>) {
+    fun matches(portfolio: Portfolio): Boolean {
+        val firstAsset = portfolio.assets[0] as Asset
+        val firstAssetPercentage = Percentage("100")
+        if (firstAsset.isin == this.values[0].isin && firstAssetPercentage == this.values[0].percentage) {
+            return true
+        }
+        return false
+    }
+}
+
 data class AssetAllocationSingle(val isin: ISIN, val percentage: Percentage)
 data class Percentage(val value: String)
