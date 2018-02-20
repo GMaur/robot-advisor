3379f8ddb2c91bef825bc6ab8365b03a2da6f5f2
Alvaro Garcia <derecerca@gmail.com>
2018-02-20 08:12:13 +0100

    Enable JSON pretty printing

--- src/main/kotlin/com/gmaur/investment/robotadvisor/infrastructure/JSONMapper.kt
+++ src/main/kotlin/com/gmaur/investment/robotadvisor/infrastructure/JSONMapper.kt
@@ -1,6 +1,7 @@
 package com.gmaur.investment.robotadvisor.infrastructure
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.databind.module.SimpleModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.gmaur.investment.robotadvisor.infrastructure.alien.r4automator.XDeserializer
@@ -9,6 +10,7 @@ class JSONMapper {
     companion object {
         fun aNew(): ObjectMapper {
             val mapper = jacksonObjectMapper()
+            mapper.enable(SerializationFeature.INDENT_OUTPUT)
             val module = SimpleModule()
             module.addDeserializer(XDTO::class.java, XDeserializer())
             mapper.registerModule(module)
