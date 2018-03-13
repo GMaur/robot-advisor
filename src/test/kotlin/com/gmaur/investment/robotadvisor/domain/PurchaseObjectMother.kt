3092ced9ca0869b6c02c1dcc2f3dfffcca47677b
Alvaro Garcia <derecerca@gmail.com>
2018-03-13 07:29:40 +0100

    Refactor: prefer using this factory method

--- src/test/kotlin/com/gmaur/investment/robotadvisor/domain/PurchaseObjectMother.kt
+++ src/test/kotlin/com/gmaur/investment/robotadvisor/domain/PurchaseObjectMother.kt
@@ -1,11 +1,9 @@
 package com.gmaur.investment.robotadvisor.domain
 
-import java.math.BigDecimal
-
 class PurchaseObjectMother {
     companion object {
         fun fund(isin: String, value: String): Purchase {
-            return Purchase(FundDefinition(ISIN(isin)), Amount(BigDecimal(value)))
+            return Purchase(FundDefinition(ISIN(isin)), Amount.EUR(value))
         }
     }
 
