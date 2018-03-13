a4ff150c785d614c9a4a386220294c3d8806defd
Alvaro Garcia <derecerca@gmail.com>
2018-03-13 08:00:32 +0100

    Refactor: extract explaining method

--- src/test/kotlin/com/gmaur/investment/robotadvisor/domain/AssetObjectMother.kt
+++ src/test/kotlin/com/gmaur/investment/robotadvisor/domain/AssetObjectMother.kt
@@ -3,12 +3,14 @@ package com.gmaur.investment.robotadvisor.domain
 class AssetObjectMother {
     companion object {
         fun cash(amountValue: String): Asset {
-            return Cash(Amount.EUR(amountValue))
+            return Cash(amount(amountValue))
         }
 
         fun fund(isinValue: String, amountValue: String): Asset {
-            return FundAsset(FundDefinition(ISIN(isinValue)), Amount.EUR(amountValue))
+            return FundAsset(FundDefinition(ISIN(isinValue)), amount(amountValue))
         }
+
+        private fun amount(amountValue: String) = Amount.EUR(amountValue)
     }
 
 }
